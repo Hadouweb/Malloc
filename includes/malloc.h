ba65ad005d7974c288c1aac5dbe53f277fcdd658
Hadouweb <kengo14@hotmail.fr>
2017-03-05 04:21:42 +0100

    Update linft

---
+++ includes/malloc.h
@@ -0,0 +1,11 @@
+#ifndef MALLOC_H
+# define MALLOC_H
+
+#include <sys/mman.h>
+#include <stdio.h>
+#include "libft.h"
+
+void show_alloc_mem(void);
+void show_alloc_mem_ex(void);
+
+#endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,6 +1,18 @@
-#include <stdio.h>
+#include "malloc.h"
+
 
 int		main(void) {
-	printf("%s\n", "Hello World");
+	void *ptr;
+
+	ptr = mmap(0, 10, PROT_READ | PROT_WRITE, MAP_ANONYMOUS, -1, 0);
+
+	ptr = "lol";
+	printf("%p\n", ptr);
+
+	int	tab[10] = {0, 23, 150, 255,
+			12, 16,  21, 42};
+
+	print_memory(tab, sizeof(tab));
+
 	return 0;
 }
